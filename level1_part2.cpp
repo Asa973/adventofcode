2c1a460ed1efcf2c6a80d16f36cd51652fe177a7
Emma C <78084421+Asa973@users.noreply.github.com>
2025-06-23 12:24:13 +0200

    test(part 2) :part 2 need to test

--- level1_part2.cpp
+++ level1_part2.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+/*
 
+
+Vector, map
+
+for compiling :)
+cpp++ -Wall -Werror -Wextra -std=c++98 <file> 
+*/
+int main()
+{
+    std::vector<int> leftList;
+    std::map<int, int> rightCount;
+    int leftNum, rightNum;
+
+    std::ifstream inputFile("input.txt");
+    if (!inputFile) {
+        std::cerr << "Error could not open input.txt" << std::endl;
+        return 1;
+    }
+
+    while (inputFile >> leftNum >> rightNum) {
+        leftList.push_back(leftNum);
+        rightCount[rightNum]++;
+    }
+
+    inputFile.close();
+
+    long long similarityScore = 0;
+    for (size_t i = 0; i < leftList.size(); ++i) {
+        int num = leftList[i];
+        similarityScore += num * rightCount[num];
+    }
+
+    std::cout << "Similarity score: " << similarityScore << std::endl;
+  
+    return 0;
+}
